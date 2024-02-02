5802afc889d036ac02b76867334146618246bb4f
Kshipra Malviya <kshipramalviya7@gmail.com>
2024-02-02 21:45:16 +0530

    Added host.

--- tourism/settings.py
+++ tourism/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ao1ugjx1&tjazb@11g^c4#jazq!*&upnn&ayx67dt2$npyz&yu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
